0f304600b4b148cb2022dc4b586831fe25c81db0
Aiman Hayat <mrs.fareedulislam@gmail.com>
2026-09-16 07:35:00 -0400

    GLAB_385.2.1

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #  GLAB 385.2.1 - Lists & Methods
 
-print('testing')
+# To create the virtual env
+# python -m venv <virtualEnvName .venv>
+
+# To activate the virtual env:
+# scripts: source .ven/Scripts/activate 
+
+# To insatll packages in an active env, use pip install
+# pip install ipykernel
